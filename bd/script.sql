72007390e79fac1b1fee9946fe8e379b56c3847b
Rodrigo Castro <rcc.dark@gmail.com>
2022-07-17 11:50:48 -0500

    actualización db
    3 espacios de indentación, simplificar enums, usar decimal en lugar de double, cambios a los nombres de algunas columnas, usar unique, algunas cosas (desafortunadamente) pueden ser null

--- bd/script.sql
+++ bd/script.sql
@@ -1,71 +1,71 @@
-CREATE DATABASE PROYECTO_INTEGRACION;
-
-USE PROYECTO_INTEGRACION;
+DROP DATABASE IF EXISTS proyecto_integracion;
+CREATE DATABASE proyecto_integracion;
+USE proyecto_integracion;
 
 CREATE TABLE profesores (
-    numero_economico INT(10) NOT NULL, 
-    apellido_paterno_profesor VARCHAR(50) NOT NULL, 
-    apellido_materno_profesor VARCHAR(50) NOT NULL, 
-    nombre_profesor VARCHAR(50) NOT NULL, 
-    correo VARCHAR(50) NOT NULL,
-    departamento ENUM('Ciencias Básicas', 'Electrónica', 'Energía', 'Materiales', 'Sistemas') NOT NULL,
-    PRIMARY KEY (numero_economico)
-  );
-  
+   profesor INT NOT NULL, 
+   apaterno TEXT NOT NULL, 
+   amaterno TEXT NOT NULL, 
+   nombre TEXT NOT NULL, 
+   correo VARCHAR(254) DEFAULT NULL,
+   depto ENUM("CB", "EL", "EN", "MA", "SI") DEFAULT NULL,
+   PRIMARY KEY (profesor)
+);
+
 CREATE TABLE ueas (
-    clave_uea INT(7) NOT NULL, 
-    nombre_uea VARCHAR(50) NOT NULL,
-    horas_de_clase DOUBLE NOT NULL, 
-    PRIMARY KEY (clave_uea)
-  );
-  
-  
+   uea INT NOT NULL, 
+   uea_nombre TEXT NOT NULL,
+   horas DECIMAL(3, 1) NOT NULL, 
+   PRIMARY KEY (uea)
+);
+
 CREATE TABLE salones (
-    id_salon INT NOT NULL AUTO_INCREMENT,
-    nombre_salon VARCHAR(50) NOT NULL, 
-    aforo_verde INT NOT NULL, 
-    aforo_amarillo INT NOT NULL, 
-    aforo_rojo INT NOT NULL, 
-    PRIMARY KEY (id_salon)
-);  
-  
-  
+   salon INT NOT NULL, 
+   salon_nombre VARCHAR(32) NOT NULL, 
+   aforo100 SMALLINT DEFAULT NULL,
+   aforo75 SMALLINT DEFAULT NULL,
+   aforo50 SMALLINT DEFAULT NULL,
+   PRIMARY KEY (salon),
+   UNIQUE (salon_nombre)
+);
+
 CREATE TABLE evaluaciones (
-    id_evaluacion INT NOT NULL AUTO_INCREMENT,
-    trimestre YEAR NOT NULL,
-    periodo ENUM('Invierno','Primavera', 'Otoño') NOT NULL,
-    tipo ENUM ('Global', 'Recuperación') NOT NULL,
-    PRIMARY KEY (id_evaluacion)
+   evaluacion INT NOT NULL AUTO_INCREMENT,
+   trimestre YEAR NOT NULL,
+   periodo ENUM("I", "P", "O") NOT NULL,
+   tipo ENUM("GLO", "REC") NOT NULL,
+   PRIMARY KEY (evaluacion)
 );
 
 CREATE TABLE grupos (
-    id_grupo INT NOT NULL AUTO_INCREMENT,
-    uea INT(7) NOT NULL, 
-    evaluacion INT NOT NULL,
-    PRIMARY KEY (id_grupo),
-    FOREIGN KEY (uea) REFERENCES ueas(clave_uea) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (evaluacion) REFERENCES evaluaciones(id_evaluacion) ON UPDATE CASCADE ON DELETE RESTRICT
+   grupo INT NOT NULL AUTO_INCREMENT,
+   uea INT NOT NULL, 
+   clave VARCHAR(32) NOT NULL,
+   evaluacion INT NOT NULL,
+   PRIMARY KEY (grupo),
+   UNIQUE (uea, clave, evaluacion),
+   FOREIGN KEY (uea) REFERENCES ueas (uea) ON DELETE CASCADE,
+   FOREIGN KEY (evaluacion) REFERENCES evaluaciones (evaluacion) ON DELETE CASCADE
 );
 
 CREATE TABLE horarios_grupo (
-    id_horario_grupo INT NOT NULL AUTO_INCREMENT, 
-    grupo INT NOT NULL,
-    salon INT NOT NULL,
-    dia ENUM('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes') NOT NULL,
-    horario_inicio TIME NOT NULL,
-    horario_fin TIME NOT NULL,
-    PRIMARY KEY (id_horario_grupo),
-    FOREIGN KEY (grupo) REFERENCES grupos(id_grupo) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (salon) REFERENCES salones(id_salon) ON UPDATE CASCADE ON DELETE RESTRICT
+   horario_grupo INT NOT NULL AUTO_INCREMENT,
+   grupo INT NOT NULL,
+   salon INT DEFAULT NULL,
+   dia ENUM("LU", "MA", "MI", "JU", "VI") DEFAULT NULL,
+   inicio TIME DEFAULT NULL,
+   termino TIME DEFAULT NULL,
+   PRIMARY KEY (horario_grupo),
+   FOREIGN KEY (grupo) REFERENCES grupos (grupo) ON DELETE CASCADE,
+   FOREIGN KEY (salon) REFERENCES salones (salon) ON DELETE CASCADE
 );
 
-
-
 CREATE TABLE profesores_grupo (
-    id_profesores_grupo INT NOT NULL AUTO_INCREMENT,
-    pg_grupo INT NOT NULL, 
-    profesor INT(10) NOT NULL, 
-    PRIMARY KEY (id_profesores_grupo),
-    FOREIGN KEY (pg_grupo) REFERENCES grupos(id_grupo) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (profesor) REFERENCES profesores(numero_economico) ON UPDATE CASCADE ON DELETE RESTRICT
+   profesor_grupo INT NOT NULL,
+   grupo INT NOT NULL, 
+   profesor INT DEFAULT NULL,
+   PRIMARY KEY (profesor_grupo),
+   UNIQUE (grupo, profesor),
+   FOREIGN KEY (grupo) REFERENCES grupos (grupo) ON DELETE CASCADE,
+   FOREIGN KEY (profesor) REFERENCES profesores (profesor) ON DELETE CASCADE
 );
