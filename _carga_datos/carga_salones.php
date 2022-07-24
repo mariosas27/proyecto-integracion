2614d8e4268e53c7eb5ebbc89e28cbb924325f26
Juan Mario Sastre Cuahutle <mariosas27@gmail.com>
2022-07-24 18:40:35 -0500

    Script que carga los datos de los salones del archivo salones.csv a la tabla salones de la base de datos

--- _carga_datos/carga_salones.php
+++ _carga_datos/carga_salones.php
@@ -1,1 +1,12 @@
 <?php
+    require_once('../includes/db_access.php');
+    require_once('../database/auth.php');
+
+    $file = fopen("salones.csv","r");
+    $conexion = new db_access(HOST_DB, USER_DB, "1234", DATABASE_DB); //el password es el que tengo en mi entorno local
+    while($fila = fgetcsv($file)){
+        $conexion->query('INSERT IGNORE INTO salones (salon, edificio, nombre, aforo100, aforo75, aforo50) VALUES (?, ?, ?, ?, ?, ?)', null, $fila[0], $fila[1], $fila[2], $fila[3], $fila[4]);
+    }
+    fclose($file);
+
+?>
