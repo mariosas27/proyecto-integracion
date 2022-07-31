9a3c51852f1aeb9357d9ff5d94a21ee54ae3950f
Rodrigo Castro <rcc.dark@gmail.com>
2022-07-30 22:00:10 -0500

    Manejo de sesión terminado

--- scripts/sesion.js
+++ scripts/sesion.js
@@ -1,18 +1,31 @@
-async function inicia_sesion(usuario = null) {
+let usuario = null;
+
+async function inicia_sesion(intento = null, redirigir_index = false) {
    try {
-      let res = await ajax_post("scripts/sesion.php", (usuario == null ? { "servicio": "estado" } : { "servicio": "entrar", "token": usuario.credential }), 3000);
-      if (!res.estado) {
-         if (res.valor == "token_invalido") {
-            window.alert("El servidor rechazó la verificación de identidad.");
-         } else {
-            window.alert("Error en la invocación del servicio. Favor de reportar el problema.");
-         }
+      let res = await ajax_post("scripts/sesion.php", (intento == null ? { "servicio": "estado" } : { "servicio": "entrar", "token": intento.credential }), 3000);
+      if (res.estado && res.valor.length != 0) {
+         usuario = res.valor;
+         document.body.classList.add("autenticado");
+         document.getElementById("usuario").innerText = res.valor.nombre;
       } else {
-         if (res.valor.email != undefined && res.valor.nombre != undefined) {
-            alert(`Entrando como:\n${res.valor.email} ${res.valor.nombre}`);
-            document.body.classList.add("autenticado");
+         if (!res.estado) {
+            window.alert((res.valor == "token_invalido" ? "El servidor rechazó la verificación de identidad." :
+                         (res.valor == "email_invalido" ? "Tu cuenta de correo no está autorizada para usar la aplicación. Verifica que estés usando una cuenta autorizada o reporta el problema." :
+                                                          "Error en la invocación del servicio. Favor de reportar el problema.")));
+         }
+         if (redirigir_index) {
+            window.location.href = "index.html";
          }
       }
+   } catch (e) {
+      console.log(ex);
+   }
+}
+
+async function termina_sesion( ) {
+   try {
+      await ajax_post("scripts/sesion.php", { "servicio": "salir" }, 3000);
+      window.location.href = "index.html";
    } catch (e) {
       console.log(ex);
    }
--- scripts/sesion.php
+++ scripts/sesion.php
@@ -8,6 +8,7 @@
 
    try {
       date_default_timezone_set('America/Mexico_City');
+      session_start( );
       $conexion = new db_access(HOST_DB, USER_DB, PASSWORD_DB, DATABASE_DB);
 
       $entrada = new input( );
@@ -20,8 +21,14 @@
       $entrada->validate($_POST, 'servicio', make_filter(match_predicate('entrar')));
       $entrada->validate($_POST, 'token', make_filter('is_string'));                 
       if ($entrada->status( )) {                                                     
-         die(json_encode(entrar_impl($conexion, $entrada->output('token'))));        
+         die(json_encode(entrar_impl($entrada->output('token'))));        
       }                                                                              
+
+      $entrada = new input( );
+      $entrada->validate($_POST, 'servicio', make_filter(match_predicate('salir')));
+      if ($entrada->status( )) {
+         die(json_encode(salir_impl( )));
+      }
 
       die(json_encode([ 'estado' => false, 'valor' => 'error_invocacion' ]));  
    } catch (Exception $ex) {                                                                 
@@ -31,16 +38,38 @@
    // implementación de casos de uso
    
    function estado_impl( ) {
-      return [ 'estado' => true, 'valor' => [ ] ];
+      return [ 'estado' => true, 'valor' => (empty($_SESSION) ? [ ] : [ 'nombre' => $_SESSION['nombre'], 'departamento' => $_SESSION['departamento'] ]) ];
    }
    
-   function entrar_impl($conexion, $token) {
+   function entrar_impl($token) {
 		$cliente = new Google_Client([ 'client_id' => '946102343979-9sbkkauvf8k7qeer1g300of8knfnu3pm.apps.googleusercontent.com' ]);
 		$cliente->setHttpClient(new \GuzzleHttp\Client([ 'curl' => [ CURLOPT_SSL_VERIFYPEER => false ] ]));
 		if (!($payload = $cliente->verifyIdToken($token))) {
          return [ 'estado' => false, 'valor' => 'token_invalido' ];
-      } else {
-         return [ 'estado' => true, 'valor' => [ 'email' => $payload['email'], 'nombre' => $payload['name'] ] ];
       }
+      
+      $autorizados = [
+         'dircbi@azc.uam.mx' => null,
+         'sacacbi@azc.uam.mx' => null,
+         'coordti@azc.uam.mx' => null,
+         'mgap@azc.uam.mx' => null,
+         'al2183041903@azc.uam.mx' => null,
+         'jdepcb@azc.uam.mx' => 'CB',
+         'jdepel@azc.uam.mx' => 'EL',
+         'jdepen@azc.uam.mx' => 'EN',
+         'jdepma@azc.uam.mx' => 'MA',
+         'jdepsi@azc.uam.mx' => 'SI',
+      ];
+      if (!array_key_exists($payload['email'], $autorizados)) {
+         return [ 'estado' => false, 'valor' => 'email_invalido' ];
+      }
+      
+      $_SESSION = [ 'nombre' => $payload['name'], 'departamento' => $autorizados[$payload['email']] ];
+		return estado_impl( );
+   }
+   
+   function salir_impl( ) {
+      $_SESSION = [ ];
+      return [ 'estado' => true, 'valor' => null ];
    }
 ?>
